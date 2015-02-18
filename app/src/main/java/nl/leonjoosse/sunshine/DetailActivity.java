c8827dac2758d94e2e12c5db2fab22123491020b
ljoosse <ljoosse@gmail.com>
2015-02-18 11:35:48 +0100

    Added share option to DetailActivity

--- app/src/main/java/nl/leonjoosse/sunshine/DetailActivity.java
+++ app/src/main/java/nl/leonjoosse/sunshine/DetailActivity.java
@@ -2,7 +2,9 @@ package nl.leonjoosse.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -10,6 +12,8 @@ import android.view.MenuItem;
  * An activity that hosts the {@link ForecastDetailFragment}.
  */
 public class DetailActivity extends ActionBarActivity {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +35,25 @@ public class DetailActivity extends ActionBarActivity {
         }
     }
 
+    private Intent getShareIntent() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(
+                Intent.EXTRA_TEXT,
+                getIntent().getStringExtra(Intent.EXTRA_TEXT) + " #Sunshine"
+        );
+        return intent;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        mShareActionProvider.setShareIntent(getShareIntent());
+
         return true;
     }
 
